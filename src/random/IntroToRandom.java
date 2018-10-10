3ab6b72734d6e7c46773eba1ce3c323ebd064ee0
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-10-09 17:57:06 -0700

    Intro to Random, Dice roll, began on Validation.

--- src/random/IntroToRandom.java
+++ src/random/IntroToRandom.java
@@ -7,23 +7,22 @@ import java.util.Random;
 
 public class IntroToRandom {
 	public static void main(String[] args) {
-		
-		int r = 0;
-		
-		//3. Now make r random. 
-		//   Create an object of the Random class
-		//	 use .nextInt() to get a random number
-		
 
-		//4. Limit the random number between 0 and 100
+		// Create an object of the Random class
+		// use .nextInt() to get a random number
+		Random k = new Random();
+		int r = k.nextInt(311) + (-222);
 
-		//5. Limit the random number between 25 and 75. Hint: ((highest value - lowest value)+1) + lowest value
+		// 4. Limit the random number between 0 and 100
 
-		//6. Challenge: Limit the random number between -222 and 88
+		// 5. Limit the random number between 25 and 75. Hint: ((highest value - lowest
+		// value)+1) + lowest value
 
-		//1. Print out the value of r
-		
-		//2. Run the program.  What number appears in the console?
-		//   Run it again. Is the number the same?
+		// 6. Challenge: Limit the random number between -222 and 88
+
+		// 1. Print out the value of r
+		System.out.println(r);
+		// 2. Run the program. What number appears in the console?
+		// Run it again. Is the number the same?
 	}
 }
--- src/random/Validation.java
+++ src/random/Validation.java
@@ -17,9 +17,48 @@ public class Validation {
 		System.out.println(randomNumber);
 
 		// 1. Use each value of randomNumber to give the user a random compliment.
-
+		for(int = 0;0<10;0++);
+		
+		
+		
+		
+		if (randomNumber == 1) {
+			
+			
+			
+			
+		} else if ( randomNumber == 2) {
+			
+						
+		}else if (randomNumber ==3) {
+			
+			
+		} else if (randomNumber ==4) {
+			
+			
+		} else if (randomNumber ==5) {
+			
+			
+		} else if (randomNumber==6) {
+			
+			
+		} else if (randomNumber ==7) {
+			
+			
+		} else if (randomNumber == 8) {
+			
+			
+		} else if (randomNumber==9) {
+			
+			
+		} else if (randomNumber==10) {
+			
+			
+		}
 		// 2. Repeat all the code above 10 times
 		
+		
 		// 3. Find someone to test out your program. They will like it :)
+		
 	}
 }
